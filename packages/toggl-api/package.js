595874e69927c23b30093b01234ea61bca078b26
Nazar Yablonskiy <nazaryablonskiy@gmail.com>
2015-06-22 17:05:09 +0300

    toggle api

---
+++ packages/toggl-api/package.js
@@ -0,0 +1,29 @@
+Package.describe({
+  name: 'jss:toggl-api',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2');
+  api.addFiles('toggl-api.js');
+});
+
+
+Npm.depends({
+  'toggl-api': '0.0.3'
+});
+
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('jss:toggl-api');
+  api.addFiles('toggl-api-tests.js');
+  api.export('TogglClient');
+});
---
+++ packages/toggl-api/toggl-api-tests.js
@@ -0,0 +1,1 @@
+TogglClient = Npm.require('toggl-api')
